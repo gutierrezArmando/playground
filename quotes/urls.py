ea1d44d2819dd4684c8c523eed52cfd6a06d2a72
Armando <37322555+gutierrezArmando@users.noreply.github.com>
2026-04-01 13:33:20 -0700

    playgrond finished

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,6 +1,9 @@
+#quotes/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<dia>', views.multiDia)
+    path("",views.index),
+    path('<int:dia>', views.parametro_int),
+    path('<str:dia>', views.parametro_str, name="dia-frase")
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,15 +1,37 @@
+#quotes/views.py
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+misFrases = {
+    "lunes":"Pricio de semana, No voy a trabajar!",
+    "martes": "Ni te cases ni te embarques, Pa' que voy a arriesgarme, No voy a trabajar",
+    "miercoles":"Se casa la patrona, Ay que pachangona, No voy a trabajar",
+    "jueves":"Estoy muy desvelado, Me siento hasta mareado, No voy a trabaja",
+    "viernes":"Que muere jesucristo, Donde se ha visto?, No voy a trabajar"
+}
+
+def index(request):
+    list_frases = ""
+    for frase in misFrases:
+        path_dia = reverse("dia-frase",args=[frase])
+        list_frases +=f'<li><a href="{path_dia}">{frase}</a></li>'
+    html_response = f"<ul>{list_frases}</ul>"
+    return HttpResponse(html_response)
+
+def parametro_str(request, dia):
+    try:
+        frase = misFrases[dia]
+        return HttpResponse(frase)
+    except KeyError:
+        return HttpResponseNotFound("Este día no existe")
+
+def parametro_int(request, dia):
+    dias = list(misFrases.keys())
+    if dia > len(dias):
+        return HttpResponseNotFound("El dá no existe")
+    redirect_dia = dias[dia-1]
+    redirect_path = reverse("dia-frase", args=[redirect_dia])
+    return HttpResponseRedirect(redirect_path)
 
 
-
-#Método que recibe un parametro
-def multiDia(request, dia):
-    text= None;
-    if dia == "lunes":
-        text = "Este es el primer día"
-    elif dia == "martes":
-        text = "Este es el segundo día"
-    else:
-        return HttpResponseNotFound("Página no encontrada")
-    return HttpResponse(text)
